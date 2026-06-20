152a8686749727ea8a3f8ffe19b0dfbb8b29b92d
Akanxi Kushwaha <akanxi@gmail.com>
2026-06-20 21:38:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 4000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/instagram',(req,res) => {
+    res.send('Instagram API endpoint');
+});
+
+app.get('/login',(req,res) => {
+    res.send('<h1> please login </h1>');
+});
+
+app.get('/youtube',(req,res)=>{
+    res.send('Youtube API endpoint');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
